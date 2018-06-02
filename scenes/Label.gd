577b51b5e9e14f3785bf24991c3160df4ff892a3
Roger Oriol Pérez <rogeroriwd@gmail.com>
2018-06-02 23:29:24 +0200

    Added license to all files

--- scenes/Label.gd
+++ scenes/Label.gd
@@ -1,3 +1,19 @@
+# TUX INVASION: a zombie mobile video game
+# Copyright (C) 2018 Roger Oriol Pérez, Èric Rodríguez Balsells, Laura Figuerola Peña
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 extends Label
 
 # class member variables go here, for example:
